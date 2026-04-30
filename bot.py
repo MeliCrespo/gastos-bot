f0101bc12c86c79219d9ff2abb499c340c38f620
MeliCrespo <melisacrespo2001@gmail.com>
2026-04-30 10:36:46 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    print(f"Mensaje recibido: {text}")
+    await update.message.reply_text("Recibido 👌")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+print("Bot corriendo...")
+app.run_polling()
